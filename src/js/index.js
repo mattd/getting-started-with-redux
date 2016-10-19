4d4860138219caa16bdc58cf3032a085a2776d0a
Matt Dawson <matthewtdawson@gmail.com>
2016-10-19 14:13:56 -0400

    Do all of exercise 6

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
 require('../scss/style.scss');
 
-console.log('Hello, world!');
+import { createStore } from 'redux';
+
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+const store = createStore(counter);
+
+const render = () => {
+    document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' });
+});
