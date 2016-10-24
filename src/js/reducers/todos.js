8cf34acb6dc27b1650960c4bc8f5c5c01a3405d1
Matt Dawson <matthewtdawson@gmail.com>
2016-10-24 11:32:48 -0400

    Start fresh on modules

---
+++ src/js/reducers/todos.js
@@ -0,0 +1,10 @@
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return;
+        default:
+            return;
+    }
+};
+
+export default todos;
--- src/js/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-
-import counter from './reducers/counter';
-
-export default createStore(counter);
