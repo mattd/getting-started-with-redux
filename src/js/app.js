878234022159e31f720d2033656f1c581d113ad5
Matt Dawson <matthewtdawson@gmail.com>
2016-10-29 23:07:57 -0400

    Remove homegrown combineReducers

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,4 @@
-// import { combineReducers } from 'redux';
-
-const combineReducers = (reducers) => {
-    return (state = {}, action) => {
-        return Object.keys(reducers).reduce(
-            (nextState, key) => {
-                nextState[key] = reducers[key](
-                    state[key],
-                    action
-                );
-                return nextState;
-            },
-            {}
-        );
-    };
-};
+import { combineReducers } from 'redux';
 
 import todos from './reducers/todos';
 import visibilityFilter from './reducers/visibility-filter';
