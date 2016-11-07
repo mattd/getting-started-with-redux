e3c1b0b7ad152f71ec908857d96e0a4f4e4884bd
Matt Dawson <matthewtdawson@gmail.com>
2016-11-07 00:58:58 -0500

    Do all of exercise 26

--- src/js/index.js
+++ src/js/index.js
@@ -3,24 +3,10 @@ require('../scss/style.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import app from './app';
 import App from './components/app';
-
-class Provider extends React.Component {
-    getChildContext() {
-        return {
-            store: this.props.store
-        };
-    }
-
-    render() {
-        return this.props.children;
-    }
-}
-Provider.childContextTypes = {
-    store: React.PropTypes.object
-};
 
 ReactDOM.render(
     <Provider store={createStore(app)}>
